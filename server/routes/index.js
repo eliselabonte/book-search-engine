58aeb61096c6dbc33e5eb38693b4596b2eb8d114
Elise_LaBonte <emlabonte714@gmail.com>
2021-11-22 18:29:50 -0500

    full application function

--- server/routes/index.js
+++ server/routes/index.js
@@ -6,6 +6,7 @@ router.use('/api', apiRoutes);
 
 // serve up react front-end in production
 router.use((req, res) => {
+  console.log(path.join(__dirname, '../../client/build/index.html'))
   res.sendFile(path.join(__dirname, '../../client/build/index.html'));
 });
 
