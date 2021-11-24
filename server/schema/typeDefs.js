23424ec5cd957d7fd653c8ac3dc3b6b4fb08b9cc
Elise_LaBonte <emlabonte714@gmail.com>
2021-11-24 14:30:34 -0500

    update Auth Middleware, add typeDefs and resolvers, update server for apollo

---
+++ server/schema/typeDefs.js
@@ -0,0 +1,32 @@
+const gql = require('graphql');
+
+const typeDefs = gql`
+    type Query {
+        me: User
+    }
+    type User   {
+        _id: String
+        username: String
+        email: String
+        bookCount: Int
+        saveBooks: [Book]
+    }
+    type Book:  {
+        bookId: String
+        authors: [String]
+        description: String
+        image: String
+        link: String
+        title: String
+    }
+    type Auth   {
+        token:String
+        user: User
+    }
+    type Mutation   {
+        login(email: String, password: String): Auth
+        addUser(username: String, email: String, password: String): Auth
+    }
+    `;
+
+module.exports = typeDefs
